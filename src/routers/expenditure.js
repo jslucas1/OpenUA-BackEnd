e92bccd35e1be490b5324b6b8ede7612a9f56c64
Jeff Lucas <jslucas1@msn.com>
2020-03-02 07:32:25 -0600

    Updated copied routes to point to expenditures

--- src/routers/expenditure.js
+++ src/routers/expenditure.js
@@ -5,86 +5,52 @@ const router = new express.Router()
 // =====================================================
 //                Expenditure flow path
 // =====================================================
-// router.post('/expenditures', (req, res) => {
-//     const myGame = new Game(req.body);
+router.post('/expenditures', (req, res) => {
+    const myExpenditure = new Expenditure(req.body);
 
-//     myGame.save().then(() => {
-//         res.status(201).send(myGame);
-//     }).catch((e) => {
-//         res.status(400).send(e);
-//     })
-// })
+    myExpenditure.save().then(() => {
+        res.status(201).send(myExpenditure);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+})
 
-// router.put('/games/:id', (req, res) => {
-//     const myGame = new Game(req.body);
-//     const myId = myGame._id;
-//     Game.findOneAndUpdate({"_id": myId}, {"$set": {"favorite": myGame.favorite,
-//                                                    "underdog": myGame.underdog,
-//                                                    "line": myGame.line,
-//                                                    "week": myGame.week,
-//                                                    "date": myGame.date,
-//                                                    "homeScore": myGame.homeScore,
-//                                                    "awayScore": myGame.awayScore,
-//                                                    "winner": myGame.winner,
-//                                                    "homeTeam": myGame.homeTeam
-//                                                 }}, 
-//         {new: true}).then((myGame) => {
-//         res.send(myGame);
-//     }).catch((e) => {
-//         res.status(500).send();
-//     })
-// })
-
-router.get('/expenditures', (req, res) => {
-    Expenditure.find({}).then((myGames) => {
-        res.send(myGames);
+router.put('/expenditures/:id', (req, res) => {
+    const myExpenditure = new Expenditure(req.body);
+    const myId = myExpenditure._id;
+    Expenditure.findOneAndUpdate({"_id": myId}, {"$set": {"favorite": myGame.favorite,
+                                                   "EXPENDITURES_DATE": myExpenditure.EXPENDITURES_DATE,
+                                                   "EXPENDITURES_SEQ": myExpenditure.EXPENDITURES_SEQ
+                                                //   continue this pattern for the other fields
+                                                }}, 
+        {new: true}).then((myExpenditure) => {
+        res.send(myExpenditure);
     }).catch((e) => {
         res.status(500).send();
     })
 })
 
-// router.get('/games/:id', (req, res) => {
-//     const _id = req.params.id;
-//     Game.findById(_id).then((myGame) => {
-//         if(!myGame) {
-//             return res.status(404).send();
-//         } 
-//         res.send(myGame)
-//     }).catch((e) => {
-//         res.status(500).send();
-//     })
+router.get('/expenditures', (req, res) => {
+    Expenditure.find({}).then((myExpenditures) => {
+        res.send(myExpenditures);
+    }).catch((e) => {
+        res.status(500).send();
+    })
+})
 
-// })
+router.get('/expenditures/:id', (req, res) => {
+    const _id = req.params.id;
+    Expenditure.findById(_id).then((myExpenditure) => {
+        if(!myExpenditure) {
+            return res.status(404).send();
+        } 
+        res.send(myExpenditure)
+    }).catch((e) => {
+        res.status(500).send();
+    })
 
-// router.get('/games/week/:week', (req, res) => {
-//     const myWeek = req.params.week;
-//     const currWeek = "2";
-//     if(myWeek == 0)
-//     { 
-//         Game.find({week: currWeek}).then((myGames) => {
-//             res.send(myGames);
-//         }).catch((e) => {
-//             res.status(500).send();
-//     })
-//     } else {
-//         Game.find({week: myWeek}).then((myGames) => {
-//             res.send(myGames);
-//         }).catch((e) => {
-//             res.status(500).send();
-//     })  
-//     }
-// })
+ })
 
-// router.delete('/games/:id', (req, res) => {
-//     const _id = req.params.id;
-//     Game.findByIdAndRemove(_id).then((myGame) => {
-//         if(!myGame) {
-//             return res.status(404).send();
-//         } 
-//         res.send(myGame)
-//     }).catch((e) => {
-//         res.status(500).send();
-//     })
-// })
+
 
 module.exports = router
