72f08e0048f99cdc9024a29031bc49982cd4c6a7
carlkramp <Carlkramp1@gmail.com>
2020-04-21 16:17:57 -0500

    Added comments to code

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,9 @@
 // Mongoose is an npm package that makes modeling data in mongodb easier
 const mongoose = require('mongoose');
 
-// Jeff's database, name of paye document is paye with single e, should be changed in future to two e's
+// This is the connection URL that was used for testing purposes, it connects to a cloud mongodb database. "m15C4pst0n3" was the password for the
+// cloud account, "@cluster0-z4nqv.azure.mongodb.net" was the cluster address that the database was in, and "TestUAOpen" was the name
+// of the database
 const connectionURL = 'mongodb+srv://OpenAdmin:m15C4pst0n3@cluster0-z4nqv.azure.mongodb.net/TestUAOpen?retryWrites=true&w=majority';
 
 // Connection string to connect to the database hosted on MongoDB, connection string uses mongoose npm package
